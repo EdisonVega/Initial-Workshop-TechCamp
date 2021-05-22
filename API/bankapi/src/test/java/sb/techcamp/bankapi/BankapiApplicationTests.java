6d0741818c3264ecab7ce67f56e3d5b7e0990321
Edison Vega <edison.vegaluc@gmail.com>
2021-05-21 20:12:05 -0500

    First commit from proyect

---
+++ API/bankapi/src/test/java/sb/techcamp/bankapi/BankapiApplicationTests.java
@@ -0,0 +1,13 @@
+package sb.techcamp.bankapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
